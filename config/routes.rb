92c53386cbb607a2f9c388b9815f51ff0f1559b9
Alex <alex.phazz@gmail.com>
2022-10-30 20:27:54 +0200

    feat: db design

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  resources :predictions
+  resources :matches
+  resources :teams
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'users/index'
+  root "users#index"
 
   # Defines the root path route ("/")
   # root "articles#index"
---
+++ db/migrate/20221030181707_alter_users.rb
@@ -0,0 +1,5 @@
+class AlterUsers < ActiveRecord::Migration[7.0]
+    def change
+        add_column :users, :age, :integer
+    end
+end
